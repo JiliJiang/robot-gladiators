acb1db739e647609bf682b0bf481b20478d72250
JiliJiang <jiangjili@gmail.com>
2021-08-11 21:37:42 -0700

    for loop added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,27 +1,15 @@
 
 var playerName = window.prompt("What is your robot's name?");
-var playerHealth = 100;
-var playerAttack = 10;
-
-console.log(playerName, playerAttack, playerHealth);
-
-
-var enemyHealth = 50;
-var enemyAttack = 12;
 
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 
 
-for (var i=0; i<enemyNames.length; i=i+1) {
-  enemyName = enemyNames[i]
-}
-
 
 
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
-    // ask player if they'd like to fight or run
+    // ask player if they'd lwhile (playerHealth > 0 && enemyHealth > 0) ike to fight or run
     var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
     // if player picks "skip" confirm and then stop the loop
@@ -73,4 +61,21 @@ var fight = function(enemyName) {
       window.alert(playerName + ' still has ' + playerHealth + ' health left.');
     }
   }
-};
+}
+
+
+for (var i=0; i<enemyNames.length; i++) 
+
+{
+  
+  var pickedEnemyName = enemyNames[i]; 
+  var enemyHealth = 50;
+  var enemyAttack = 12;
+  var playerHealth = 100;
+  var playerAttack = 10;
+  var playerMoney =10;
+
+  fight(pickedEnemyName);
+  
+}
+
